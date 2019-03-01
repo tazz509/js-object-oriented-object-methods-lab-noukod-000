7ff3ee20564833bdb2d92035b1467f4a0970ed9d
carly <carly@noukod.com>
2019-03-01 21:02:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-function BoardMember (name, homestate, training){
-  this.name = name;
-  this.homestate = homestate;
-  this.training = training
-  this.veto = function(){
-    console.log(`No, I must disagree`);
-  }
-    this.approve = function(){
-      console.log('You can do that!');
-    }
-    this.doCharity = function(){
-      console.log('I like to help people.');
-    }
-    this.releasePressStatement = function(){
-      console.log('You will see great things from Scuber.');
-    }
-    this.sayHi = function(){
-      console.log('Hi, my name is ${name}. I am from ${homestate}, and I was trained in {training}.');
-    }
+function BoardMember(name, homeState, training){
+    this.name = name; this.homeState = homeState;
+    this.training = training;
 
+    this.veto = function(){ return 'No, I must disagree'};
+    this.approve  = function(){ return 'You can do that!'};
+    this.doCharity  = function(){ return 'I like to help people.'};
+    this.releasePressStatement  = function(){ return 'You will see great things from Scuber.'};
+    this.sayHi = function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`};
 }
